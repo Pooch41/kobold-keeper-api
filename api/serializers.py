a473bd993189e56082d263e8bea2a31e2bc3e2b9
Pooch41 <d.kulikovski@gmail.com>
2025-10-07 19:56:06 +0200

    Serialiser - 2

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
-from api.models import Group, Roll, User
+from api.models import Group, Roll, User, Character
 
 class GroupSerializer(serializers.ModelSerializer):
     owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
     class Meta:
         model = Group
         fields = ['id', 'group_name']
+
+
+class CharacterSerializer(serializers.ModelSerializer):
+    group = serializers.PrimaryKeyRelatedField(queryset=Group.objects.all())
+
+    class Meta:
+        model = Character
+        fields = [
+            'id',
+            'character_name',
+            'character_note',
+            'is_npc',
+            'group'
+        ]
+        read_only_fields = ['id']
