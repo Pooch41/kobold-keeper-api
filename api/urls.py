7d0e6509ad5b6932e7f18ce15b5a433fe163f924
Pooch41 <d.kulikovski@gmail.com>
2025-10-13 12:49:52 +0200

    URL Update

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,24 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from . import views
+from .viewsets import GroupViewSet, CharacterViewSet, RollViewSet, UserRegistrationViewSet
+from .authentication import CustomTokenObtainPairView, PasswordResetWithKeyView
+
+router = DefaultRouter()
+
+router.register(r'groups', GroupViewSet, basename='group')
+router.register(r'characters', CharacterViewSet, basename='character')
+router.register(r'roll-history', RollViewSet, basename='roll-history')
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('docs/', views.docs, name='docs'),
-    path('login/', views.login, name='login'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('password-reset/', PasswordResetWithKeyView.as_view(), name='password_reset'),
+
+    path('register/', UserRegistrationViewSet.as_view({'post': 'create'}), name='user_register'),
+
+    path('roll/', RollViewSet.as_view({'post': 'create'}), name='create_roll'),
+
+    path('', include(router.urls)),
 ]
--- kobold_keeper/urls.py
+++ kobold_keeper/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api import views as web_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls'))
+
+    path('', web_views.home, name='home'),
+    path('about', web_views.about, name='about'),
+    path('docs', web_views.docs, name='docs'),
+    path('login', web_views.login, name='login'),
+
+    path('api', include('api.urls')),
 ]
