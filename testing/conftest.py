82e685fec7e4b142991a6e52085d423c26bccdd4
Pooch41 <d.kulikovski@gmail.com>
2025-10-17 18:24:18 +0200

    testing

---
+++ testing/conftest.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import django
+from django.conf import settings
+from pathlib import Path
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+sys.path.append(str(PROJECT_ROOT))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kobold_keeper.settings')
+
+
+def pytest_configure(config):
+    """
+    Pytest hook that runs once at the start of the test session.
+    It performs the full Django setup.
+    """
+    try:
+        if not settings.configured:
+            django.setup()
+    except Exception as e:
+        print(f"Error during Django setup: {e}")
+
+
+def pytest_unconfigure(config):
+    """
+    Optional cleanup hook.
+    """
+    pass
